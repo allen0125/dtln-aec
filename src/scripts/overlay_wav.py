938be6ac490eb778df9115044aab3b2018d7a0d1
wy <wy@yashihq.ccom>
2022-01-11 17:49:45 +0800

    User black

--- src/scripts/overlay_wav.py
+++ src/scripts/overlay_wav.py
@@ -1,24 +1,29 @@
 import os
 from pydub import AudioSegment
 
-mix_output_dir = '/Users/allen/Project/YaShi/aec/data/origin_data/mix/bgm-45'
+mix_output_dir = "/Users/allen/Project/YaShi/aec/data/origin_data/mix/bgm-45"
 
 
 def loudnorm(audio_segment: AudioSegment, db: float) -> AudioSegment:
     _change = db - audio_segment.dBFS
     return audio_segment.apply_gain(_change)
 
+
 def mix_mic_lpb(base):
     for _, _, fs in os.walk(base):
         for f in fs:
-            if f.endswith('-mic.wav'):
+            if f.endswith("-mic.wav"):
                 print(f)
-                lpb_f = f.replace('-mic', '-lpb')
+                lpb_f = f.replace("-mic", "-lpb")
                 mic_audio_segment = AudioSegment.from_wav(os.path.join(base, f))
-                lpb_audio_segment = loudnorm(AudioSegment.from_wav(os.path.join(base, lpb_f)), -45)
+                lpb_audio_segment = loudnorm(
+                    AudioSegment.from_wav(os.path.join(base, lpb_f)), -45
+                )
                 mix_audio = mic_audio_segment.overlay(lpb_audio_segment, position=0)
-                output_file_name = f.replace('-mic', '-mix-bgm-45')
-                mix_audio.export(os.path.join(mix_output_dir, output_file_name), format='wav')
+                output_file_name = f.replace("-mic", "-mix-bgm-45")
+                mix_audio.export(
+                    os.path.join(mix_output_dir, output_file_name), format="wav"
+                )
 
 
-mix_mic_lpb('/Users/allen/Project/YaShi/aec/data/origin_data/wav')
+mix_mic_lpb("/Users/allen/Project/YaShi/aec/data/origin_data/wav")
